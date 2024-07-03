e51cc91c8059782a80024540ef6660da8c1403b4
Airton Alves Medina <blindarcher@outlook.com>
2024-07-03 05:04:44 -0300

    Add files via upload

---
+++ DIOToken.sol
@@ -0,0 +1,97 @@
+// https://eips.ethereum.org/EIPS/eip-20
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+
+    function totalSupply() external view returns(uint256);
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return balance the balance
+    function balanceOf(address _owner) external view returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return success Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value)  external returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return success Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return success Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) external returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return remaining Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract DIOToken is IERC20 {
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public constant name = "DIO Token";   // Set the name for display purposes
+    string public constant symbol = "DIO";      // Set the symbol for display purposes
+    uint8 public constant decimals = 18;        // Amount of decimals for display purposes
+
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
+    uint256 public _totalSupply = 10 ether;
+
+    constructor() {
+        balances[msg.sender] = _totalSupply;       // Give the creator all initial tokens
+    }
+
+    function totalSupply() public override view returns(uint256) {
+        return _totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public override returns (bool success) {
+        require(balances[msg.sender] >= _value, "token balance is lower than the value requested");
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
+        require(balances[_from] >= _value, "token balance is lower than amount requested");
+        require(allowed[_from][msg.sender] >= _value, "token allowance is lower than amount requested");
+
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address _owner) public override view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public override returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public override view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+} 
